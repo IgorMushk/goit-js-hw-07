6db75e0e8b82314374526723d9ffc6388517b247
IgorMushk <igor.mushketik@gmail.com>
2023-07-06 16:27:10 +0300

    Exercise-02 - // wrap in a function cteateMarkupGallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -23,19 +23,20 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 //console.log(galleryItems);
-//console.log("GitHub");
+console.log("GitHub +");
 
-const imagesListArr = galleryItems.map(({ preview, original, description }) => {
-  return `<li class="gallery__item">
+function cteateMarkupGallery(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img class="gallery__image" src="${preview}" alt="${description}" />
   </a>
 </li>`;
-});
+  });
+}
 
 const galleryList = document.querySelector(".gallery");
-galleryList.insertAdjacentHTML("beforeend", imagesListArr.join(""));
-
+galleryList.insertAdjacentHTML("beforeend", cteateMarkupGallery(galleryItems).join(""));
 galleryList.addEventListener("click", handleImgClick);
 
 function handleImgClick(event) {
